59f584ddedbb292d7c203b1cd7a286efa5ba39e4
Dhruv-J <dhruvj@vmware.com>
2025-01-02 11:26:03 -0800

    unit test fixes, copyright fix
    Signed-off-by: Dhruv-J <dhruvj@vmware.com>

--- pkg/agent/apiserver/handlers/fqdncache/handler.go
+++ pkg/agent/apiserver/handlers/fqdncache/handler.go
@@ -1,10 +1,22 @@
+// Copyright 2025 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package fqdncache
 
 import (
-	"bytes"
 	"encoding/json"
 	"net/http"
-	"reflect"
 
 	"k8s.io/klog/v2"
 
@@ -13,21 +25,13 @@ import (
 
 func HandleFunc(aq agentquerier.AgentQuerier) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		klog.InfoS("DBUG: fqdn cache handler HandleFunc func called")
 		dnsEntryCache := aq.GetFqdnCache()
 		if dnsEntryCache == nil {
 			return
 		}
-		klog.InfoS("DBUG: dns entry cache", "obj", dnsEntryCache)
 		if err := json.NewEncoder(w).Encode(dnsEntryCache); err != nil {
 			http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
 			klog.ErrorS(err, "Failed to encode response")
 		}
-		buf := bytes.Buffer{}
-		if err := json.NewEncoder(&buf).Encode(dnsEntryCache); err != nil {
-			http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
-			klog.ErrorS(err, "Failed to encode response")
-		}
-		klog.InfoS("DBUG:", "type", reflect.TypeOf(dnsEntryCache), "buf", buf.String())
 	}
 }
